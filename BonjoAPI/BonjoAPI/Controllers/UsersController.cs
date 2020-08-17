2e1e4f8ab013b6e8039337cd0f2decd0ba6e70ba
roizdnaB <danieljambor1999@gmail.com>
2020-08-18 01:50:16 +0200

    Added some movie models and automapper profiles

---
+++ BonjoAPI/BonjoAPI/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BonjoAPI.Others;
+using BonjoAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BonjoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+        private readonly IMapper mapper;
+        private readonly AppSettings appSettings;
+
+        public UsersController(IUserService userService, IMapper mapper, AppSettings appSettings)
+        {
+            this.userService = userService;
+            this.mapper = mapper;
+            this.appSettings = appSettings;
+        }
+    }
+}
--- BonjoAPI/BonjoAPI/Models/Movie/MovieRegisterModel.cs
+++ BonjoAPI/BonjoAPI/Models/Movie/MovieRegisterModel.cs
@@ -1,7 +1,10 @@
-﻿namespace BonjoAPI.Models.Movie
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BonjoAPI.Models.Movie
 {
     public class MovieRegisterModel
     {
+        [Required]
         public string Title { get; set; }
     }
 }
