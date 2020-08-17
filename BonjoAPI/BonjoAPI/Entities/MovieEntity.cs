50448b514931d922e6c05b926a516cf6a72a444f
roizdnaB <danieljambor1999@gmail.com>
2020-08-18 00:54:47 +0200

    Added user and movie entities

---
+++ BonjoAPI/BonjoAPI/Entities/MovieEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace BonjoAPI.Entities
+{
+    public class MovieEntity
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ BonjoAPI/BonjoAPI/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace BonjoAPI.Entities
+{
+    public class UserEntity
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
