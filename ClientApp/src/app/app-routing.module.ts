f5ecfc182b3d0996bec459f0f5a0239f22fd3f15
roizdnaB <danieljambor1999@gmail.com>
2020-08-18 23:01:02 +0200

    Updated app-routing module

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { AuthGuard } from './others/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { HomeComponent } from './components/home/home.component';
+import { StartComponent } from './components/start/start.component';
+
+const routes: Routes = [
+  {path: "", component: StartComponent, canActivate: [AuthGuard]},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'home', component: HomeComponent},
+
+  {path: "**", redirectTo: ""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
