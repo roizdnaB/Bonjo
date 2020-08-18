8af250252ac8ec8d5e6da4196adc35cd7b9d597a
roizdnaB <danieljambor1999@gmail.com>
2020-08-18 22:31:55 +0200

    Added jwt interceptor, authentication service

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { AuthenticationService } from './services/authentication.service';
+import { User } from './models/userModel';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ClientApp';
+  title = 'Bonjo';
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x)
+  }
 }
---
+++ ClientApp/src/app/others/jwt.interceptor.ts
@@ -0,0 +1,4 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
