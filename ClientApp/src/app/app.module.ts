9d534b03ac32b79e7a9384d33fdca4f4d14cc6ae
roizdnaB <danieljambor1999@gmail.com>
2020-08-18 22:56:52 +0200

    Added components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { JwtInterceptor } from './others/jwt.interceptor';
+
 import { AppComponent } from './app.component';
+import { StartComponent } from './components/start/start.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ClientApp/src/app/others/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@Angular/router';
+
+import { AuthenticationService } from '../services/authentication.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {}
+
+    canActivate(
+        activatedRouteSnapshot: ActivatedRouteSnapshot,
+        routerStateSnapshot: RouterStateSnapshot) {
+            const currentUser = this.authenticationService.currentUserValue;
+            if (currentUser) {
+                return true;
+            }
+
+            this.router.navigate(['/home'], { queryParams: { returnUrl: state.url }});
+            return false;
+        }
+}
